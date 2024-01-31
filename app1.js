c168b6159d9fc9265e29a1fac8bac30140ecd1b2
Mohib <mohibullahmohim2020@gmail.com>
2024-02-01 01:49:43 +0600

    First Commit to add

---
+++ app1.js
@@ -0,0 +1,8 @@
+let numbers = [23, 24, 11, 44, 56, 66];
+
+document.write( 'Before pushing element - '+ numbers +'<br>');
+
+numbers.push(233);
+
+document.write( 'After pushing element - '+ numbers +'<br>');
+
