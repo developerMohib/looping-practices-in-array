5eb4b2af581296102ddbc93af24d20ed4fd30326
Mohib <mohibullahmohim2020@gmail.com>
2024-02-01 01:59:34 +0600

    second commit

--- app1.js
+++ app1.js
@@ -1,8 +1,15 @@
-let numbers = [23, 24, 11, 44, 56, 66];
+let numbers = [43, 32, 44, 8, 23, 3];
 
 document.write( 'Before pushing element - '+ numbers +'<br>');
 
-numbers.push(233);
+
+for(let i = 1; i <= 5 ; i++){
+    numbers.push(Number(prompt(`Enter Your Number to add in Numbers Array :`)));
+}
+
 
 document.write( 'After pushing element - '+ numbers +'<br>');
 
+for(let nums of numbers){
+    document.write( 'After looping element - '+ nums +'<br>');
+}
